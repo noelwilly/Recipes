05eac689827aa982b4560ec1442334f61b46effb
NoelWilliam <noelwilliamm@gmail.com>
2024-04-04 22:34:36 +0200

    Big copy/paste moment

---
+++ modules/settings.js
@@ -0,0 +1,2 @@
+export const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBleHdib2FpZW92cm94amtjenNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ1MDEwNzgsImV4cCI6MjAxMDA3NzA3OH0.6BHSqoC3DM7BXM7_X4Syq5W3u_U5MKO7EPClIkefExs";
+export const endpoint = "https://pexwboaieovroxjkczsc.supabase.co/rest/v1/Recipes";
